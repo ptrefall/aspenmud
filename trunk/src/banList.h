644527d68897777c90fabc20a585acbf4d64ee97
tyler@tysdomain.com <tyler@tysdomain.com@e0fecb69-7791-3f99-eeb7-292fcc47c875>
2011-07-09 15:29:45 +0000

    Moved the ban list to a separate file.

---
+++ trunk/src/banList.h
@@ -0,0 +1,21 @@
+#ifndef BAN_LIST_H
+#define BAN_LIST_H
+#include "mud.h"
+#include "conf.h"
+#include "serializer.h"
+
+class BanList:public ISerializable
+{
+  std::vector<unsigned long> _addresses;
+public:
+  BanList();
+  ~BanList();
+  BOOL AddAddress(const std::string &address);
+  BOOL RemoveAddress(const std::string &address);
+  BOOL AddressExists(const std::string &address);
+  BOOL AddressExists(unsigned long address);
+  void ListAddresses(std::vector<std::string> *addresses);
+  void Serialize(TiXmlElement* root);
+  void Deserialize(TiXmlElement* root);
+};
+#endif
